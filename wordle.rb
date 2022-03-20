2cfe067f135acd1eb3421742bc8e6dfb3c1479b9
Nidhi Sarvaiya <sarvaiya.nidhi@gmail.com>
2022-03-20 00:36:18 -0400

    Get user input for answer for number of valid attempts

--- wordle.rb
+++ wordle.rb
@@ -1,12 +1,29 @@
 class Wordle
 
   def game
-    @words = word_list
-    @total_turns = 6
-    @wordle_view = Array.new (@total_turns) {"_ _ _ _ _"}
+    words = word_list
+    total_turns = 6
+    wordle_view = Array.new (total_turns) {"_ _ _ _ _"}
+
+    puts "Let's start the game"
+    puts wordle_view
     
     # Selecting random word as answer from the list of available words
-    @answer = @words[rand(@words.size - 1)].upcase
+    answer = words[rand(words.size - 1)].upcase
+
+    @player_turn = 0
+    @player_answer = ""
+
+    while @player_turn < total_turns && @player_answer != answer do
+      puts "Please guess your #{@player_turn + 1} word"
+      @player_answer = gets.chomp().upcase
+
+      if @player_answer.empty? || @player_answer.size != 5
+        puts "Invalid word! Please enter valid 5 letter word"
+      else
+        @player_turn += 1
+      end
+    end
 
   end
 
