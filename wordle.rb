2cef60a0d803e60b7815254ab1492da1c007f8f8
Nidhi Sarvaiya <sarvaiya.nidhi@gmail.com>
2022-03-20 01:04:53 -0400

    Validating answer and printing in grid

--- wordle.rb
+++ wordle.rb
@@ -3,24 +3,29 @@ class Wordle
   def game
     words = word_list
     total_turns = 6
-    wordle_view = Array.new (total_turns) {"_ _ _ _ _"}
+    @wordle_view = Array.new (total_turns) {"_ _ _ _ _"}
 
-    puts "Let's start the game"
-    puts wordle_view
+    puts "Let's start the game \n"
+    puts @wordle_view
     
     # Selecting random word as answer from the list of available words
-    answer = words[rand(words.size - 1)].upcase
+    @answer = words[rand(words.size - 1)].upcase
+    puts @answer
 
     @player_turn = 0
     @player_answer = ""
 
-    while @player_turn < total_turns && @player_answer != answer do
+    while @player_turn < total_turns && @player_answer != @answer do
       puts "Please guess your #{@player_turn + 1} word"
       @player_answer = gets.chomp().upcase
 
       if @player_answer.empty? || @player_answer.size != 5
         puts "Invalid word! Please enter valid 5 letter word"
       else
+        validate_answer(@player_answer, @answer)
+        @wordle_view[@player_turn] = @player_answer
+        puts "Your answer \n"
+        puts @wordle_view
         @player_turn += 1
       end
     end
@@ -30,6 +35,13 @@ class Wordle
   def word_list
     word_list = []
     words = File.readlines("dictionary.txt", chomp: true)
+  end
+
+  def validate_answer(player_answer, answer)
+
+    if player_answer == answer
+      player_answer
+    end
   end
 
 end
