d17a5467a2b9e0b4ae596170940d178293138e72
Nidhi Sarvaiya <sarvaiya.nidhi@gmail.com>
2022-03-19 23:37:14 -0400

    Generated randon answer from available words for the game

--- wordle.rb
+++ wordle.rb
@@ -2,6 +2,12 @@ class Wordle
 
   def game
     @words = word_list
+    @total_turns = 6
+    @wordle_view = Array.new (@total_turns) {"_ _ _ _ _"}
+    
+    # Selecting random word as answer from the list of available words
+    @answer = @words[rand(@words.size - 1)].upcase
+
   end
 
   def word_list
